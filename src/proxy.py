03db7ecb15799bcccdc68f0fecf1e94788b69cb9
Affan Shaikh <shaikhaffan1234@gmail.com>
2026-05-22 03:22:42 +0530

    feat: add logic for:
    1. Send request upstream
    
    2. Handle timeout and connection error
    
    3. Create response object in case of successful response
    
    4. Measure response time

--- src/proxy.py
+++ src/proxy.py
@@ -1,11 +1,9 @@
 import time
-from tracemalloc import start
-from types import BuiltinMethodType
 
 import httpx
 
 from src.middleware.base import Middleware
-from src.models import MiddlewareContext, MiddlewareResult
+from src.models import MiddlewareContext, MiddlewareResult, ProxyResponse
 
 
 class ProxyMiddleware(Middleware):
@@ -25,7 +23,7 @@ class ProxyMiddleware(Middleware):
             request_path = curr_request.path[len(curr_route.path_prefix) :]
             final_upstream_URL = curr_route.upstream_URL + request_path
         else:
-            final_upstream_URL = curr_route.upstream_URL
+            final_upstream_URL = curr_route.upstream_URL + curr_request.path
 
         # remove hop-by-hop headers defined by RFC 2616 (world-wide standard)
         client_headers = curr_request.headers
@@ -52,15 +50,42 @@ class ProxyMiddleware(Middleware):
         forward_headers["X-Forwarded-For"] = curr_request.client_ip
         forward_headers["X-Forwarded-Proto"] = "https"
 
-        start_time = time.perf_counter()
+        # actually sending the request upstream
+        try:
+            start_time = time.perf_counter()
 
-        response = await self.client.request(
-            method=curr_request.method,
-            url=final_upstream_URL,
-            headers=forward_headers,
-            content=curr_request.body,
+            response = await self.client.request(
+                method=curr_request.method,
+                url=final_upstream_URL,
+                headers=forward_headers,
+                content=curr_request.body,
+            )
+
+        except httpx.TimeoutException:
+            context.abort_response = {"status_code": 504, "detail": "Request Timeout"}
+
+            return MiddlewareResult.ABORT
+
+        except httpx.ConnectError:
+            context.abort_response = {
+                "status_code": 502,
+                "detail": "Could Not Connect To Server",
+            }
+
+            return MiddlewareResult.ABORT
+
+        # measuring response time
+        end_time = time.perf_counter()
+        curr_response_time = (end_time - start_time) * 1000
+
+        # building the response object
+        context.response = ProxyResponse(
+            request_id=curr_request.request_id,
+            headers=dict(response.headers),
+            body=response.content,
+            status_code=response.status_code,
+            response_time=curr_response_time,
+            from_cache=False,
         )
 
-        end_time = time.perf_counter()
-
-        response_time = (end_time - start_time) * 1000
+        return MiddlewareResult.PASS
