d67cf04ee7c898ecc4c0b68f8f3f8b5c0ac3ddfa
Affan Shaikh <shaikhaffan1234@gmail.com>
2026-05-31 21:37:04 +0530

    feat: add InMemoryStorage class.

---
+++ src/storage/memory.py
@@ -0,0 +1,35 @@
+import time
+from typing import Any, Optional
+
+from src.storage.base import Storage
+
+
+class InMemoryStorage(Storage):
+    def __init__(self):
+        self.store = {}
+        self.lifetime = {}
+
+    async def get_value(self, key: str) -> Optional[Any]:
+        return self.store.get(key)
+
+    async def set_value(self, key: str, value: Any, expire_in: int) -> None:
+        self.store[key] = value
+        self.lifetime[key] = time.time() + expire_in
+
+    # this is atomic since there is no 'await'
+    async def increment_value(self, key: str, expire_in: int) -> int:
+        if key not in self.store.keys() or time.time() > self.lifetime[key]:
+            # we initialise to 1 as thats how Redis would initialise a new val.
+            self.store[key] = 1
+            self.lifetime[key] = time.time() + expire_in
+            return 1
+
+        else:
+            self.store[key] += 1
+            return self.store[key]
+
+    # need to see the Lua script before implementing
+    async def execute_script(
+        self, script: str, keys: list[str], args: list[Any]
+    ) -> Any:
+        raise NotImplementedError()
