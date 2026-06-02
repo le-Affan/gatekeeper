af83fffeac7f0dca647c6e8a88d5bc6f00ded665
Affan Shaikh <shaikhaffan1234@gmail.com>
2026-06-02 21:24:18 +0530

    feat: add storage and made identifier namespaced

--- src/middleware/rate_limiter.py
+++ src/middleware/rate_limiter.py
@@ -3,7 +3,8 @@ from src.models import MiddlewareContext, MiddlewareResult
 
 
 class RateLimiter(Middleware):
-    def __init__(self, algorithm: str, api_key_headers : list[str]):
+    def __init__(self, algorithm: str, api_key_headers: list[str], storage: str):
+        self.storage = storage
         self.algorithm = algorithm
         self.api_key_headers = api_key_headers
 
@@ -12,8 +13,9 @@ class RateLimiter(Middleware):
         return "rate-limiter"
 
     async def process(self, context: MiddlewareContext) -> MiddlewareResult:
+
+        # identify client API Key else fallback to IP Address
         identifier = None
-        
         for header in self.api_key_headers:
             identifier = context.request.headers.get(header)
 
@@ -23,11 +25,7 @@ class RateLimiter(Middleware):
         if not identifier:
             identifier = context.request.client_ip
 
+        # creating namespaced key
+        identifier = f"rate-limiter:{identifier}"
+
         
-                
-            
-
-
-
-            
-                
